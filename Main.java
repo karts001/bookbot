03ebc9d5327509fae265f1bb429adcc83e1f5dda
karts001 <karts001@gmail.com>
2022-12-18 15:50:57 +0000

    moved functionality into separate methods

--- Main.java
+++ Main.java
@@ -6,23 +6,32 @@ import java.lang.String;
 
     public class Main {
     public static void main(String[] args) {
-    
         String filePathToBook = "/home/shiva/workspace/github.com/karts001/bookbot/books/frankenstein.txt";
-        Path filePath = Path.of(filePathToBook);
+        Path filePath = openTheFile(filePathToBook);
+        String fileContent = readTheContentsOfTheFile(filePath);
+        int numberOfWordsInTheBook = numberOfWordsInTheString(fileContent);
+        
+        System.out.printf("There are %s words in the Frankenstein book", numberOfWordsInTheBook);       
+        }
+
+    public static Path openTheFile(String filePath) {
+        Path path = Path.of(filePath);
+        return path;
+    }
+
+    public static String readTheContentsOfTheFile(Path filePath) {
         String fileContent = "";
-        int numberOfWordsInTheBook = 0;
         try {
             // constructor of file class having file as argument
             fileContent = Files.readString(filePath);
-            numberOfWordsInTheBook = numberOfWordsInTheString(fileContent);
+            
         }
         catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
             }
-        
-        System.out.printf("There are %s words in the Frankenstein book", numberOfWordsInTheBook);        
-        }
+        return fileContent;
+    }
 
     public static int numberOfWordsInTheString(String stringToEvaluate) {
         if (stringToEvaluate == null || stringToEvaluate.isEmpty()) {
